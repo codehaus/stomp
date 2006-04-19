c132bd31592763faa974d5d8504ad9cc1857098d
chirino <chirino@fd4e7336-3dff-0310-b68a-b6615a75f13b>
2006-04-19 17:38:05 +0000

    - Fixed a small issue with tx test. - Connection can be set to be reliable so that on socket failure, the client does not see error, but silently   goes into a reconnect loop. - Added some simple little utilities called catstomp and stompcat and act a little like netcat, but for stomp.

--- ruby/test/test_client.rb
+++ ruby/test/test_client.rb
@@ -164,16 +164,17 @@ class TestClient < Test::Unit::TestCase
 
     @client.begin 'tx1'
     message = nil
-    @client.subscribe(destination, :ack => 'client') {|m| message = m}
-    sleep 0.01 until message
+    @client.subscribe(destination, :ack => 'client') { |m| message = m }
+    
+    sleep 0.1 while message == nil
+    
     assert_equal message_text, message.body
     @client.acknowledge message, :transaction => 'tx1'
     message = nil
     @client.abort 'tx1'
 
-    Timeout::timeout(4) do 
-      sleep 0.01 until message
-    end
+    sleep 0.1 while message == nil
+    
     assert_not_nil message    
     assert_equal message_text, message.body
 
