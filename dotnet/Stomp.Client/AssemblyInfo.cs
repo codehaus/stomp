3a0bc1fd46f3811677981d9df629b194e9b5fe12
jstrachan <jstrachan@fd4e7336-3dff-0310-b68a-b6615a75f13b>
2007-03-02 16:24:34 +0000

    [maven-scm] copy for tag stompconnect-1.0

--- dotnet/Stomp.Client/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Stomp Client")]
-[assembly: AssemblyDescription("")]
-
-[assembly: AssemblyVersion("0.1.*")]
